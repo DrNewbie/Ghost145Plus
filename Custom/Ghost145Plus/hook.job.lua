deb1d3c0655ff4d93df47f6d3b95699eb785dba8
Dr_Newbie <tf2baidonation@gmail.com>
2017-11-11 05:54:48 +0800

    .

--- Custom/Ghost145Plus/hook.job.lua
+++ Custom/Ghost145Plus/hook.job.lua
@@ -1,9 +1,6 @@
 function JobManager:_set_ghost_bonus(ghost_bonus, digest)
 	ghost_bonus = Application:digest_value(ghost_bonus, false)
 	ghost_bonus = ghost_bonus or 0
-	if m_ghost._data.iidetect == 2 then
-		ghost_bonus = ghost_bonus + 99
-	end
 	if not m_ghost._data.ecmcam then
 		ghost_bonus = ghost_bonus + 3
 	end
@@ -42,6 +39,13 @@ function JobManager:_set_ghost_bonus(ghost_bonus, digest)
 		ghost_bonus = ghost_bonus + 7
 	elseif m_ghost._data.shoutingnoise == 5 then
 		ghost_bonus = ghost_bonus + 15
+	end
+	if m_ghost._data.iidetect == 2 then
+		ghost_bonus = ghost_bonus + 10
+	end
+	ghost_bonus = math.clamp(ghost_bonus, 0, 50)
+	if m_ghost._data.iidetect == 2 and m_ghost._data.shoutingnoise == 5 and m_ghost._data.markduration == 5 and m_ghost._data.pager == 5 and m_ghost._data.nocivkill and not m_ghost._data.ecmca and not m_ghost._data.ecmphone and not m_ghost._data.ecmpager then
+		ghost_bonus = 999
 	end
 	ghost_bonus = Application:digest_value(ghost_bonus, true)
 	self._global.saved_ghost_bonus = digest and Application:digest_value(ghost_bonus, true) or ghost_bonus
